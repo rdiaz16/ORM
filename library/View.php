08ae7fbd17f41aa6096cbaca827b0ea675e200a3
Ricardo Diaz <ricardodiaz.0201@gmail.com>
2023-05-29 20:14:45 -0700

    passing controller data to view

--- library/View.php
+++ library/View.php
@@ -2,7 +2,7 @@
 
 
 class View {
-    public static function create($path){
+    public static function create($path,$key=null,$value=null){
     //check if the path variable exists
     if($path != ""){
         $paths = explode(".",$path);//its converted to a point-separated array
@@ -20,6 +20,17 @@ class View {
         //echo VIEW_ROUTE.$route;
         //check if the file exists
         if(file_exists(VIEW_ROUTE.$route)){
+            //check that the $key exists
+            if(!is_null($key)){
+               if(is_array($key)){
+                   //Extract key value and convert to variable
+                   extract($key,EXTR_PREFIX_SAME,"");
+               }else{
+                   //("$index","$users",$users)
+                   //$users = $users;
+                   ${$key}=$value;
+               }
+            }
             include VIEW_ROUTE.$route;
         }else{
             die("the view does not exists");
@@ -28,3 +39,4 @@ class View {
     }
 
 }
+return null;
