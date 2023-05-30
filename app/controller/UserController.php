b64335a5698ada9a88eedace4370f3e7ac15ec3d
Ricardo Diaz <ricardodiaz.0201@gmail.com>
2023-05-29 20:11:37 -0700

    adding users array and improving method

--- app/controller/UserController.php
+++ app/controller/UserController.php
@@ -3,7 +3,26 @@
 use view\View;
 class UserController {
     public function index(){
-       return View::create("users.list");
+        $users = array(
+            1=>array(
+                "name"=>"Ricardo",
+                "lastname"=>"Diaz",
+            ),
+            2=>array(
+                "name"=>"Ricardo",
+                "lastname"=>"Diaz",
+            ),
+            3=>array("name"=>"Ricardo",
+                "lastname"=>"Diaz",
+            ),
+            4=>array("name"=>"Ricardo",
+                "lastname"=>"Diaz",
+            )
+        );
+       //return View::create("users.list","users",$users);
+        return View::create("users.list","users",array(
+            "users"=>$users,
+        ));
     }
     public function insert(){
         echo "inserted correctly";
