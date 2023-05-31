baa496d73590783b2bc5f22a4524d788cb2e2a23
Ricardo Diaz <ricardodiaz.0201@gmail.com>
2023-05-31 00:22:29 -0700

    replacing all includes with require_once excluding Route.php, ROUTE.routes.php

--- library/core.php
+++ library/core.php
@@ -1,5 +1,5 @@
 <?php
-include "help/helps.php";
+require_once ("help/helps.php");
 define("APP_ROUTE",BASE_ROUTE."app/");
 //echo APP_ROUTE;
 define("VIEW_ROUTE",BASE_ROUTE."view/");
@@ -8,14 +8,14 @@ define("ROUTE",APP_ROUTE."routes/");
 define("MODELS",APP_ROUTE."model/");
 //echo ROUTE;
 //configuration
-include BASE_ROUTE."config/config.php";
+require_once BASE_ROUTE."config/config.php";
 require_once ("ORM/Connection.php");
-require_once "ORM/RdORM.php";
-include "ORM/Model.php";
+require_once ("ORM/RdORM.php");
+require_once ("ORM/Model.php");
 
 
 includeModels();
-include "View.php";
+require_once ("View.php");
 include "Route.php";
 include ROUTE."routes.php";
 
