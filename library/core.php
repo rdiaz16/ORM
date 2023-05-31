fad209229c111dd976333aac02a2fa3a31de5f26
Ricardo Diaz <ricardodiaz.0201@gmail.com>
2023-05-30 18:55:50 -0700

    include ORM/Model.php and add new route Connection

--- library/core.php
+++ library/core.php
@@ -1,9 +1,19 @@
 <?php
+include "help/helps.php";
 define("APP_ROUTE",BASE_ROUTE."app/");
 //echo APP_ROUTE;
 define("VIEW_ROUTE",BASE_ROUTE."view/");
+define("LIBRARY",BASE_ROUTE."library/");
 define("ROUTE",APP_ROUTE."routes/");
+define("MODELS",APP_ROUTE."model/");
 //echo ROUTE;
+//configuration
+include BASE_ROUTE."config/config.php";
+include LIBRARY."ORM/Connection.php";
+include "ORM/Model.php";
+
+
+includeModels();
 include "View.php";
 include "Route.php";
 include ROUTE."routes.php";
