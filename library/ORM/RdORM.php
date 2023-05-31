1bcc35e219f01588dd12be2e03de211f427a900c
Ricardo Diaz <ricardodiaz.0201@gmail.com>
2023-05-31 01:36:24 -0700

    add function for retrieving the connection.

--- library/ORM/RdORM.php
+++ library/ORM/RdORM.php
@@ -4,5 +4,16 @@ namespace library\ORM;
 
 
 class RdORM extends \Connection{
+    protected static  $cnx;
+    protected static $table;
+
+    function __construct()
+    {
+        self::getConnection();//execute each time the class is invoked by means of an object
+    }
+    public  static function getConnection(){
+        self::$cnx= \Connection::connect();
+    }
+
 
 }
