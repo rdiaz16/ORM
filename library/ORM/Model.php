9b241d31c19ffdd44445460dcf9516c8eea73587
Ricardo Diaz <ricardodiaz.0201@gmail.com>
2023-05-31 01:33:07 -0700

    add private  and protected static , magic methos _get() _set()

--- library/ORM/Model.php
+++ library/ORM/Model.php
@@ -7,6 +7,7 @@ namespace library\ORM;
 class Model extends RdORM {
     //property that will contain all properties dynamically
     private $data=array();
+    protected static $table;
 
     function __construct($data = null)
     {
